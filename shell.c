4db3d4c8bd15b134f7dd0205e5a80712dc36ba90
Andresromeroh <andres.romero22@outlook.com>
2019-03-16 15:33:54 -0600

    Implementation of Background Processing Flag

--- shell.c
+++ shell.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <regex.h>
 #include <stdio.h>
 #include <wait.h>
 
-#define PROMPT "Shell> "
+#define PROMPTCOLOR  "\x1B[1;32m" 
 #define MAX_CHARACTERS 1024
 #define NUM_BUILT_IN_CMDS 2
 
 static char* builtInCmds[NUM_BUILT_IN_CMDS]; //Array of Built-In Commands
-
+static int executionFlag = 1; //Flag for executing the main() process
+static int backgroundProcessFlag = 0; //Specifies if "&" is present
 
 #pragma region FUNCTIONS:
 void initShell(){
@@ -29,9 +31,16 @@ char** parseCommand(char* command){
 
     while(token != NULL) //Break command into single parts, Ex: ls -l -> [ls] [-l]
     {
-        parsedCommand[iterator] = token;             
-        iterator ++;                          
-        token = strtok(NULL, " \t\r\n\a");  
+        // If & is included process running in background:
+        if(strcmp(token, "&") == 0){
+            backgroundProcessFlag = 1;
+            iterator ++;                          
+            token = strtok(NULL, " \t\r\n\a");  
+        } else {
+            parsedCommand[iterator] = token;             
+            iterator ++;                          
+            token = strtok(NULL, " \t\r\n\a");  
+        }
     }
 
     parsedCommand[iterator] = NULL;
@@ -39,7 +48,7 @@ char** parseCommand(char* command){
     return parsedCommand;
 }
 
-int executeCommand(char** parsedCommand){
+int executeCommand(char** parsedCommand, char backgroundProcessIndicator){
     pid_t pid;
     pid = fork();
 
@@ -48,8 +57,9 @@ int executeCommand(char** parsedCommand){
         return -1;
     }
     else if (pid == 0) {
-        //execlp(command,command,NULL);
-        execvp(parsedCommand[0], parsedCommand);
+        if (execvp(parsedCommand[0], parsedCommand) == -1) {
+            printf("%s: command not found\n", parsedCommand[0]);
+        }
     }
     else {
         wait(NULL);
@@ -86,22 +96,22 @@ int main(int argc, char const *argv[])
 {
     char* command = malloc(sizeof(char) * MAX_CHARACTERS);
     char** parsedCommand = malloc (sizeof(char *) * MAX_CHARACTERS);
-    int executionFlag = 1;
 
     initShell(); // Executing init() function...
 
     while (executionFlag == 1) { 
 
-        printf("%s",PROMPT); // Print Shell PROMPT
-        fflush (stdout); // Clear (or flush) the output buffer
+        printf("%sShell> \x1B[37m",PROMPTCOLOR); // Print Shell PROMPT
+        fflush(stdout); // Clear (or flush) the output buffer
         fgets(command,MAX_CHARACTERS,stdin); // Get user input
-        command[strcspn(command, "\n")] = '\0'; // Remove 'ENTER'
 
         if (strlen(command) > 1) {
 
+            command[strcspn(command, "\n")] = '\0'; // Remove 'ENTER'
+
             if (builtInHandler(command) == 0) {
-                parsedCommand = parseCommand(command);
-                executeCommand(parsedCommand);
+                parsedCommand = parseCommand(command); // Break command into single parts, Ex: ls -l -> [ls] [-l]
+                executeCommand(parsedCommand, backgroundProcessFlag); //Ex: cat ejemplo.c & or ls -l etc...
             }
             
         }
