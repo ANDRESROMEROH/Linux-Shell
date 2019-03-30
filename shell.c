5bbda3e3e240785a989565cc00d3f5336144ea12
Andresromeroh <andres.romero22@outlook.com>
2019-03-30 08:28:01 -0600

    Pipes Fully Working %100

--- shell.c
+++ shell.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <regex.h>
 #include <stdio.h>
-#include <wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define PROMPTCOLOR  "\x1B[1;32m" 
 #define MAX_CHARACTERS 1024
@@ -12,107 +13,16 @@
 static char* builtInCmds[NUM_BUILTINCMDS]; //Array of Built-In Commands
 static int backgroundProcessFlag = 0; //Specifies if "&" is present
 
-
-#pragma region FUNCTIONS:
-void initShell(){// Initiate Built-In commands:
-    builtInCmds[0] = "exit"; 
-    builtInCmds[1] = "history";
-    printf("\033[H\033[J"); //Clear Screen
-}
-
-char** parseSingleCommand(char* command){
-    char **parsedCommand = malloc (sizeof(char *) * 1024);
-    char *token = malloc (sizeof(char) * MAX_CHARACTERS);
-    int iterator = 0;
-
-    token = strtok(command," \t\r\n\a");
-
-    while(token != NULL) //Break command into single parts, Ex: ls -l -> [ls] [-l]
-    {
-        // If & is included process running in background:
-        if(strcmp(token, "&") == 0){
-            backgroundProcessFlag = 1;
-            iterator ++;                          
-            token = strtok(NULL, " \t\r\n\a");  
-        } else {
-            parsedCommand[iterator] = token;             
-            iterator ++;                          
-            token = strtok(NULL, " \t\r\n\a");  
-        }
-    }
-
-    parsedCommand[iterator] = NULL;
-
-    return parsedCommand;
-}
-
-int executeSingleCommand(char** parsedCommand){
-    pid_t pid;
-    pid = fork();
-
-    if (pid < 0) {
-        printf("There was an error calling fork()\n");
-        return -1;
-    }
-    else if (pid == 0) {
-        if (execvp(parsedCommand[0], parsedCommand) == -1) {
-            printf("%s: command not found\n", parsedCommand[0]);
-            exit(EXIT_SUCCESS);
-        }
-    }
-    else {
-        if(backgroundProcessFlag != 1) { // Waiting for any child process to finish...
-            wait(NULL);
-            wait(NULL); 
-        }
-    }
-
-    return 0;
-}
-
-// Divide command if it is compose by a pipe "|":
-int divideComposedCommand(char* command, char** parsedFullCommand) 
-{ 
-    for (size_t i = 0; i < 2; i++) { 
-        parsedFullCommand[i] = strsep(&command, "|"); 
-        if (parsedFullCommand[i] == NULL) 
-            break; 
-    } 
-  
-    if (parsedFullCommand[1] == NULL) 
-        return 0; // returns 0 if no pipe is found
-    else { 
-        return 1; // returns 1 if a pipe is found
-    } 
-} 
-
-int builtInHandler(char* command){
-    int builtInCommand;
-
-    for(size_t i = 0; i < NUM_BUILTINCMDS; i++)
-    {
-        if (strcmp(command, builtInCmds[i]) == 0) { 
-            builtInCommand = i + 1; 
-            break; 
-        } 
-    }
-    
-    switch (builtInCommand)
-    {
-        case 1: //Exit
-           exit(0); // Finish the main() process
-
-        default:
-            break;
-    }
-
-    return 0;
-}
+//Functions:
+void initShell();
+char** parseSingleCommand(char* command);
+int divideComposedCommand(char* command, char** parsedFullCommand);
+int executeSingleCommand(char** parsedCommand);
+int executeCommandWithPipe(char** parsedCommand, char** parsedPipeCommand);
+int builtInHandler(char* command);
 
 
-
-/* Main Function: */
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]) /* Main Function: */
 {
     int isRunning = 1;
 
@@ -138,21 +48,160 @@ int main(int argc, char const *argv[])
 
                 if (divideComposedCommand(command, parsedFullCommand) == 1) { //If a pipe is found:
 
+                    parsedCommand = parseSingleCommand(parsedFullCommand[0]);
                     parsedPipeCommand = parseSingleCommand(parsedFullCommand[1]);
-                    executeSingleCommand(parsedPipeCommand); //Ex: cat ejemplo.c & or ls -l etc...
+                    executeCommandWithPipe(parsedCommand, parsedPipeCommand); //Ex: cat LICENSE | grep copyright
 
                 }
-                else { // If no pipe is found:
+                else { // If NO pipe is found:
 
                     parsedCommand = parseSingleCommand(parsedFullCommand[0]); 
-                    executeSingleCommand(parsedCommand); //Ex: cat ejemplo.c & or ls -l etc...
+                    executeSingleCommand(parsedCommand); //Ex: cat LICENSE & or ls -l etc...
 
                 }
             }
             
         }
     } 
+
+    return 0;
+}
+
+
+/* Funtions Implementation: */
+
+void initShell() {// Initiate Built-In commands:
+    builtInCmds[0] = "exit"; 
+    builtInCmds[1] = "history";
+    printf("\033[H\033[J"); //Clear Screen
+}
+
+
+char** parseSingleCommand(char* command) {
+    char **parsedCommand = malloc (sizeof(char *) * 1024);
+    char *token = malloc (sizeof(char) * MAX_CHARACTERS);
+    int iterator = 0;
+
+    token = strtok(command," \t\r\n\a");
+
+    while(token != NULL) //Break command into single parts, Ex: ls -l -> [ls] [-l]
+    {
+        // If & is included process running in background:
+        if(strcmp(token, "&") == 0){
+            backgroundProcessFlag = 1;
+            iterator ++;                          
+            token = strtok(NULL, " \t\r\n\a");  
+        } else {
+            parsedCommand[iterator] = token;             
+            iterator ++;                          
+            token = strtok(NULL, " \t\r\n\a");  
+        }
+    }
+
+    parsedCommand[iterator] = NULL;
+
+    return parsedCommand;
+}
+
+
+int divideComposedCommand(char* command, char** parsedFullCommand) 
+{ 
+    // Divide command if it is compose by a pipe "|":
+    for (size_t i = 0; i < 2; i++) { 
+        parsedFullCommand[i] = strsep(&command, "|"); 
+        if (parsedFullCommand[i] == NULL) 
+            break; 
+    } 
+  
+    if (parsedFullCommand[1] == NULL) 
+        return 0; // returns 0 if no pipe is found
+    else { 
+        return 1; // returns 1 if a pipe is found
+    } 
+} 
+
+
+int executeSingleCommand(char** parsedCommand){
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0) {
+        printf("There was an error calling fork()\n");
+        return -1;
+    }
+    else if (pid == 0) {
+        if (execvp(parsedCommand[0], parsedCommand) == -1) {
+            printf("%s: command not found\n", parsedCommand[0]);
+            exit(EXIT_SUCCESS);
+        }
+    }
+    else {
+        if(backgroundProcessFlag != 1) { // Waiting for any child process to finish...
+            wait(NULL);
+            wait(NULL); 
+        }
+    }
+
+    return 0;
+}
+
+
+// Execute piped system commands
+int executeCommandWithPipe(char** parsedCommand, char** parsedPipeCommand) 
+{ 
+   pid_t pid1, pid2;
+   int pipefd[2];
+
+   pipe(pipefd); // Create a pipe
+   pid1 = fork(); // Create first process
+
+   if (pid1 == 0) {
+      dup2(pipefd[1], STDOUT_FILENO);
+      close(pipefd[0]);
+      execvp(parsedCommand[0], parsedCommand);
+
+      return 1;
+   }
+
+   pid2 = fork(); // Create second process
+
+   if (pid2 == 0) {
+      dup2(pipefd[0], STDIN_FILENO);
+      close(pipefd[1]);
+      execvp(parsedPipeCommand[0], parsedPipeCommand);
+      
+      return 1;
+   }
+   
+   close(pipefd[0]); // Close reading
+   close(pipefd[1]); // Close writing
+   
+   wait(NULL);
+   wait(NULL);
+
+   return 0;
+}
+
+
+int builtInHandler(char* command){
+    int builtInCommand;
+
+    for(size_t i = 0; i < NUM_BUILTINCMDS; i++)
+    {
+        if (strcmp(command, builtInCmds[i]) == 0) { 
+            builtInCommand = i + 1; 
+            break; 
+        } 
+    }
     
+    switch (builtInCommand)
+    {
+        case 1: //Exit
+           exit(0); // Finish the main() process
+
+        default:
+            break;
+    }
 
     return 0;
 }
